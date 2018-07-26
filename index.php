d517186741c370bf90740fffa61157641a525f13
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:27:23 +0200

    variable

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>exo6</title>
+  </head>
+  <body>
+    <p>
+    <?php
+    $name = 'adeline' ;
+    echo "bonjour $name ,comment vas tu ?";
+     ?>
+   </p>
+  </body>
+</html>
